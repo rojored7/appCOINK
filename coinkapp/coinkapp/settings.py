c85f3cbe82cba19c20c316be227ce5b235c9ee2a
Rojored <u1802563@gmail.com>
2022-08-19 20:53:38 -0500

    Creación de la clase de usuario donde se tendran las diferentes variables que se utilizaran en cada caso solicitado

--- coinkapp/coinkapp/settings.py
+++ coinkapp/coinkapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "formulario.apps.FormularioConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- coinkapp/formulario/models.py
+++ coinkapp/formulario/models.py
@@ -1,3 +1,17 @@
+import email
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+        The variables to be used in the solved order are defined.
+        Name: With a maximum space of 100 characters
+        Email: With a maximum space of 100 characters
+        city: With a maximum space of 100 characters
+    """
+    name_user = models.CharField(max_length=100)
+    emai = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
