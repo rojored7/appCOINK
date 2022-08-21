d52f0f39efe2ddd5ce07bf102eb38782b5646b1a
Rojored <u1802563@gmail.com>
2022-08-21 15:51:20 -0500

    Prueba de creacion de usuario completada

--- coinkapp/formulario/tests.py
+++ coinkapp/formulario/tests.py
@@ -5,21 +5,12 @@ from urllib import response
 from .models import User
 # Create your tests here.
 
-
-class MultipleUser(TestCase):
-    
-    
-    def test_was_published_same_register(self):
-        
-        
-        repetido = User(name_user="COINK",emai="COINK", ciudad="COINK")
-        self.assertIs(repetido(),False)
+#Pruena de creacion de usuario 
+def User_creatio():
+    user=User.objects.create_user(
+        name_user="RED",
+        emai="RED@RED.com",
+        ciudad="REDPOLIS"    
+    )
+    assert user.name_user == "RED"
  
-class FormularioIndexViewTests(TestCase):
-
-    def test_no_user(self):
-        response = self.client.get(reverse("polls:index"))
-        self.assertEqual(response.status_code, 200)
-        #self.assertContains(response,text= "No polls are avaible.")
-        self.assertQuerysetEqual(response.context["lastest_question_list"],[])
-               
