60aad94ebe25f336e75c063e252cc9b7cce27386
Rojored <u1802563@gmail.com>
2022-08-21 16:42:26 -0500

    correcion sintaxis test

--- coinkapp/formulario/tests.py
+++ coinkapp/formulario/tests.py
@@ -1,16 +1,24 @@
+import datetime
+from urllib import response
+
 from django.test import TestCase
 from django.urls.base import reverse
 
 from urllib import response
 from .models import User
-# Create your tests here.
 
+# Create your tests here.
+def test_etUpTestData(cls):
+    print("setUpTestData: Run once to set up non-modified data for all class methods.")
+    pass
 #Pruena de creacion de usuario 
-def User_creatio():
-    user=User.objects.create_user(
-        name_user="RED",
-        emai="RED@RED.com",
-        ciudad="REDPOLIS"    
-    )
-    assert user.name_user == "RED"
- 
+class Creacion_usuario(TestCase):
+    
+    def test_User_creatio(self):
+        
+        user = User.objects.create(
+            name_user="RED",
+            emai="RED@RED.com",
+            ciudad="REDPOLIS" 
+        )
+        self.assertTrue(user.name_user == "RED")
